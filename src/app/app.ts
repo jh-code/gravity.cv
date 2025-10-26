577381aea24979883640d692540d8d81d10aab94
jh-code <jh.wphax@gmail.com>
2025-10-26 15:06:14 -0500

    copy and icon updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faTrophy, faPenNib, faCode, faFaceSmileBeam } from '@fortawesome/free-solid-svg-icons';
+
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Header],
+  imports: [RouterOutlet, RouterLink, Header, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('gravity.cv');
+  protected readonly icons = {
+    faCoffee, faTrophy, faPenNib, faCode, faFaceSmileBeam
+  };
 }
